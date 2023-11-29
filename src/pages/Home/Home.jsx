cde36ad4e8039067aad8762f4f2c3a1936dd34cc
AkshayB99 <akshaybharamshetti99@gmail.com>
2023-11-29 22:52:06 +0530

    added category page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+
+  return (
+    <>
+      <div className="bg-color">
+        <div className="main-container">
+          <div className="user-info">user</div>
+          <div className="all-notes">group2</div>
+          <div className="info-about">group3</div>
+          <div className="weather">weather</div>
+          <div className="timer">group5</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
