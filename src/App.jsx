47815b361cc62ac94f14319d3b76722d07f0ab62
AkshayB99 <akshaybharamshetti99@gmail.com>
2023-12-08 19:01:51 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Registration from "./pages/registration/Registration.jsx";
 import Category from "./pages/category/SelectCategory.jsx";
-
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Entertainment from "./pages/entertainment/Entertainments.jsx";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
