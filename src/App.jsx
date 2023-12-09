c2b293230ec5e43c702a3c5b1d9ea85e90ef185b
AkshayB99 <akshaybharamshetti99@gmail.com>
2023-12-09 09:45:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import Registration from "./pages/registration/Registration.jsx";
 import Category from "./pages/category/SelectCategory.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Entertainment from "./pages/entertainment/Entertainments.jsx";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Registration />} />
-        <Route path="/select" element={<Category />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/entertainment" element={<Entertainment />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Registration />} />
+          <Route path="/select" element={<Category />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/entertainment" element={<Entertainment />} />
+        </Routes>
+      </Router>
     </>
   );
 };
